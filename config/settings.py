fc2681ec6f6c5bde9b4db84fa66d274781ac72f0
MukhammadjonArabov <muhammadjonarabov04@gmail.com>
2026-05-05 12:30:51 +0500

    update settings

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    '164.92.183.18',
+    'localhost',
+    '127.0.0.1'
+]
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
